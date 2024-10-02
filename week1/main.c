b985548ef560f18d221b92f3a3fddf0db6b86136
Sauleh Eetemadi (Computer Science in Dubai) <s.eetemadi@bham.ac.uk>
2024-10-02 14:58:17 +0400

    update

--- week1/main.c
+++ week1/main.c
@@ -1,4 +1,15 @@
+#include<stdio.h>
 int main()
 {
+    int a;
+    int b;
+    int c;
+    int buf[10];
+    printf("%d, %d, %d\n", a, b, c);
+    buf[5] = 12;
+    printf("%d\n", buf[5]);
+    int* ptr = &(buf[0]);
+    ptr += 5;
+    printf("%d\n", *ptr);
     return 0;
 }
